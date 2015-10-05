3a10bc640435a15c0afcbf73414157bd568bc9a8
Tyom Semonov <inspiro@gmail.com>
2015-10-05 11:44:49 +0100

    Include Mojular base

---
+++ assets/scripts/mojular.js
@@ -0,0 +1,34 @@
+var $ = require('jquery');
+var forOwn = require('lodash/object/forOwn');
+
+(function(){
+  'use strict';
+
+  window.Mojular = {
+    Modules: {},
+    Helpers: {},
+    Events: $({}),
+
+    init: function () {
+      forOwn(this.Modules, function(module) {
+        typeof module.init === 'function' && module.init()
+      });
+
+      // trigger initial render event
+      this.Events.trigger('render');
+    },
+
+    // safe logging
+    log: function (msg) {
+      if (window && window.console) {
+        window.console.log(msg);
+      }
+    },
+
+    dir: function (obj) {
+      if (window && window.console) {
+        window.console.dir(obj);
+      }
+    }
+  };
+}());
