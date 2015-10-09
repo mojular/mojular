2a0d7ef2c6d45a3892ece1484e8aae20fc6e045e
Tyom Semonov <inspiro@gmail.com>
2015-10-09 16:22:25 +0100

    Ensure Mojular is initialised only once

--- assets/scripts/mojular.js
+++ assets/scripts/mojular.js
@@ -1,38 +1,37 @@
 var $ = require('jquery');
 var forOwn = require('lodash/object/forOwn');
 
-(function(){
-  'use strict';
+window.Mojular = window.Mojular || {
+  isInitialised: false,
+  Modules: {},
+  Helpers: {},
+  Events: $({}),
 
-  window.Mojular = {
-    Modules: {},
-    Helpers: {},
-    Events: $({}),
+  init: function () {
+    forOwn(this.Modules, function(module) {
+      typeof module.init === 'function' && module.init()
+    });
 
-    init: function () {
-      forOwn(this.Modules, function(module) {
-        typeof module.init === 'function' && module.init()
-      });
+    // trigger initial render event
+    this.Events.trigger('render');
 
-      // trigger initial render event
-      this.Events.trigger('render');
-    },
+    this.isInitialised = true;
+  },
 
-    // safe logging
-    log: function (msg) {
-      if (window && window.console) {
-        window.console.log(msg);
-      }
-    },
-
-    dir: function (obj) {
-      if (window && window.console) {
-        window.console.dir(obj);
-      }
+  // safe logging
+  log: function (msg) {
+    if (window && window.console) {
+      window.console.log(msg);
     }
-  };
+  },
 
-  $(function() {
-    window.Mojular.init();
-  })
-}());
+  dir: function (obj) {
+    if (window && window.console) {
+      window.console.dir(obj);
+    }
+  }
+};
+
+$(function() {
+  !window.Mojular.isInitialised && window.Mojular.init();
+})
