c681ff2d678e07f277ff1035122ccd1efe19ae87
Tyom Semonov <inspiro@gmail.com>
2015-11-09 15:38:45 +0000

    Fix compatibility with NPM 3

--- sass-paths.js
+++ sass-paths.js
@@ -1,42 +1,24 @@
+var fs = require('fs');
 var isArray = require('lodash/lang/isArray');
+var uniq = require('lodash/array/uniq');
+var flatten = require('lodash/array/flatten');
 var util = require('util');
 
-function prefixPaths(paths, packageName) {
-  return paths.map(function(path) {
-    return util.format('node_modules/%s/%s', packageName, path);
-  });
-}
-
-function processPackageMeta(packageMeta) {
-  if (!packageMeta) {
-    console.log('WARNING:', 'Mojular component is missing package.json reference');
+module.exports = function(packageMetas) {
+  if (!isArray(packageMetas)) {
+    console.log('WARNING:', 'Mojular component is missing package.json references',
+      '\ne.g. `require(\'mojular/sass-paths\')([require(\'mojular-govuk-elements/package.json\')');
     return;
   }
-  var packageName = packageMeta.name;
-  try {
-    var sassPaths = packageMeta.paths.sass;
-    if (!sassPaths) {
-      console.log('WARNING:', '`sass` property should exist in `paths` property of', packageName);
-      return;
-    }
-    if (!isArray(sassPaths)) {
-      console.log('WARNING:', '`paths` property in ', packageName, 'should be an array of paths');
-      return;
-    }
-    return prefixPaths(sassPaths, packageName);
-  } catch (err) {
-    console.log('ERROR:', 'Something went wrong.');
-  }
-}
 
-module.exports = function(packageMeta) {
-  var loadPaths = [];
-  if(isArray(packageMeta)) {
-    packageMeta.forEach(function(p) {
-      loadPaths.push(processPackageMeta(p));
+  var sassPaths = packageMetas.map(function(meta) {
+    return meta.sassPaths.map(function(path) {
+      if(fs.existsSync(path)) {
+        return path;
+      }
+      return util.format('node_modules/%s/%s', meta.name, path);
     });
-  } else {
-    loadPaths.push(processPackageMeta(packageMeta));
-  }
-  return [].concat.apply([], loadPaths);
+  });
+
+  return uniq(flatten(sassPaths));
 };
